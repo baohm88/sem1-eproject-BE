57f337ec2f6a513a72874da95d8f9f0b44cf6bc8
Ha Manh Bao <131301257+baohm88@users.noreply.github.com>
2024-10-17 15:37:19 +0700

    added RatingModel and RatingController

--- app/controllers/ValidateController.php
+++ app/controllers/ValidateController.php
@@ -15,7 +15,7 @@ class ValidateController
             case "username":
                 $regex = "/^[a-z0-9_]{1,30}$/";
                 break;
-            case "product_price":
+            case "price":
                 $regex =  '/(?<!\w)([€$£]|VND)?\s?\d{1,3}(?:[.,]\d{3})*(?:[.,]\d{2})?(?!\w)/';
                 break;
             case 'qty':
